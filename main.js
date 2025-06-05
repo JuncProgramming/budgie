3935b5e7d7ea04fe8c53e8e71821c7538fe609c4
Oliwier Juncewicz <oli.junc@gmail.com>
2025-06-05 06:13:15 +0200

    Wrote the delete function and modified deleteAll function with confirm alert. Hid the balance when there are no items, added currency to the list items

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ const addTransaction = (e) => {
     name,
     amount,
     type: typeValue,
+    id: Date.now(),
   };
   saveTransaction(transaction);
   transactionNameInput.value = '';
@@ -42,10 +43,12 @@ const updateUI = () => {
   while (ul.firstChild) ul.removeChild(ul.firstChild);
 
   if (transactions.length === 0) {
+    balance.style.display = 'none';
     listHeader.textContent = 'No transactions yet';
     ul.style.display = 'none';
     deleteAllBtn.style.display = 'none';
   } else {
+    balance.style.display = 'block';
     listHeader.textContent = 'List of transactions';
     ul.style.display = 'flex';
     deleteAllBtn.style.display = 'block';
@@ -53,9 +56,12 @@ const updateUI = () => {
 
   transactions.forEach((transaction) => {
     const item = document.createElement('li');
+    item.dataset.id = transaction.id;
     const icon = document.createElement('i');
     icon.classList.add('fa-solid');
-    transaction.type === 'income' ? icon.classList.add('fa-circle-up') : icon.classList.add('fa-circle-down')
+    transaction.type === 'income'
+      ? icon.classList.add('fa-circle-up')
+      : icon.classList.add('fa-circle-down');
     const div = document.createElement('div');
     div.classList.add('transaction-info');
     const name = document.createElement('span');
@@ -68,21 +74,40 @@ const updateUI = () => {
     div.appendChild(name);
     div.appendChild(amount);
 
-
     name.textContent = transaction.name;
-    amount.textContent = transaction.amount;
-
-    ul.appendChild(item);
+    (amount.textContent = new Intl.NumberFormat('pl-PL', {
+      style: 'currency',
+      currency: 'PLN',
+    }).format(transaction.amount)),
+      ul.appendChild(item);
   });
 };
 
+const deleteItem = (e) => {
+  if (e.target.closest('li')) {
+    const transactions = getTransactions();
+    if(confirm('Are you sure you want to delete this transaction?')) {
+    const filteredTransactions = transactions.filter(
+      (transaction) =>
+        transaction.id !== Number(e.target.closest('li').dataset.id)
+    );
+    localStorage.setItem('transactions', JSON.stringify(filteredTransactions));
+    updateUI();
+  } }else {
+    return;
+  }
+};
+
 const deleteAll = () => {
+  if(confirm('Are you sure you want to delete all transactions?')) {
   localStorage.removeItem('transactions');
   ul.querySelectorAll('li').forEach((li) => li.remove());
   updateUI();
+  }
 };
 
 form.addEventListener('submit', addTransaction);
+ul.addEventListener('click', deleteItem);
 deleteAllBtn.addEventListener('click', deleteAll);
 
 const init = () => {
